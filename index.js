78a38fd0ed2ba08f2ad66c62a092732c0944a812
JoviAndrew <jovian_andrew@hotmail.com>
2018-03-15 12:16:03 +0700

    submit callback multiple files

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
+
+function match_data(parent_file, children_file, cbParent, cbChild) {
   // your code here...
+  fs.readFile(parent_file, 'utf8', function(err, dataParent) {
+    cbParent(dataParent, children_file, cbChild);
+  })
+  
 }
 
-match_data('./parents.json', './childrens.json')
+function getParentData(dataParent, children_file, cbChild){
+  fs.readFile(children_file,'utf8',function(err, dataChild){
+    cbChild(dataParent, dataChild);
+  });
+
+}
+
+function getChildrenData(dataParent, dataChild){
+  let parsedDataParent = JSON.parse(dataParent);
+  let parsedDataChildren = JSON.parse(dataChild);
+
+  for(let indexParent = 0; indexParent<parsedDataParent.length; indexParent++){
+    let arrRes = [];
+    for(let indexChildren =0; indexChildren<parsedDataChildren.length; indexChildren++){
+      if(parsedDataParent[indexParent].last_name == parsedDataChildren[indexChildren].family){
+        arrRes.push(parsedDataChildren[indexChildren].full_name);
+      }
+    }
+    parsedDataParent[indexParent].childrens = arrRes; 
+  }
+  console.log(parsedDataParent);
+
+}
+
+match_data('./parents.json', './childrens.json', getParentData, getChildrenData)
+
 console.log("Notification : Data sedang diproses !");
