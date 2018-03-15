b5b0a6f125eff05dec92dbd5e26a50de5a85d26c
Rafie Gilang <gilang0293@gmail.com>
2018-03-15 17:43:24 +0700

    sudah selesai callback-multiple-file

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ const fs = require('fs');
 var sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // your code here...
+  let get_parents_obj = fs.readFile(parent_file, 'utf8',function(err, parents){
+    parents = JSON.parse(parents);
+    let childrens = fs.readFileSync(children_file, 'utf8');
+    childrens = JSON.parse(childrens);
+    for(let indexParents in parents){
+      parents[indexParents].childrens = [];
+      for(let indexChildrens in childrens){
+        if(parents[indexParents].last_name === childrens[indexChildrens].family){
+          //console.log(childrens[indexChildrens]);
+          parents[indexParents].childrens.push(childrens[indexChildrens].full_name);
+        }
+      }
+    }
+    getParents(parents);
+  });
 }
+
+function getParents(parents){
+  console.log(parents);
+}
+
 
 match_data('./parents.json', './childrens.json')
 console.log("Notification : Data sedang diproses !");
+sleep.sleep(5);
