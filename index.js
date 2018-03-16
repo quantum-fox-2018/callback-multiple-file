3391538ead7fab36fa67773af6453b1c71a4c458
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-03-16 13:24:06 +0700

    sudah make callback

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ function match_data(parent_file, children_file,cbParentFile) {
   // your code here...
   fs.readFile(parent_file,'utf8', (err,parentDatas) => {
     sleep.sleep(5);
+    var parent_datas = JSON.parse(parentDatas)
+    fs.readFile(children_file,'utf8',(err,children_datas)=>{
 
-    var childrensData = JSON.parse(fs.readFileSync(children_file,'utf8'));
-    parent_datas = JSON.parse(parentDatas);
-
-    for(let i=0;i<parent_datas.length;i++){
-      parent_datas[i].childrens = [];
-      for(let j=0;j<childrensData.length;j++){
-        if(parent_datas[i].last_name == childrensData[j].family){
-          parent_datas[i].childrens.push(childrensData[j].full_name)
+      let childrensData = JSON.parse(children_datas);
+      for(let i=0;i<parent_datas.length;i++){
+        parent_datas[i].childrens = [];
+        for(let j=0;j<childrensData.length;j++){
+          if(parent_datas[i].last_name == childrensData[j].family){
+            parent_datas[i].childrens.push(childrensData[j].full_name)
+          }
         }
       }
-    }
-    parentDatas = parent_datas;
-    cbParentFile(parentDatas);
+      parentDatas = parent_datas;
+      cbParentFile(parentDatas);
+    })
+
+
+
   })
 }
 
