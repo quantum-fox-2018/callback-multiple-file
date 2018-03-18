6901f75493d565fd3e2c89dcedbda4e426053af4
annisaayu <annisaayu569@gmail.com>
2018-03-19 00:25:51 +0700

    multipleFile

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ var sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   // your code here...
+  fs.readFile(parent_file, 'utf8', (err, data) => {
+    sleep.sleep(5);
+    let parentsFile = JSON.parse(data);
+    fs.readFile(children_file, 'utf8', (err, ChildrenData) => {
+      sleep.sleep(5);
+      let childrensFile = JSON.parse(ChildrenData);
+      for(let i=0; i<parentsFile.length; i++){
+        parentsFile[i].children = [];
+        for(let j=0; j< childrensFile.length; j++){
+          if(parentsFile[i].last_name === childrensFile[j].family){
+            parentsFile[i].children.push(childrensFile[j].full_name);
+          }
+        }
+      }
+      console.log(parentsFile);
+    })
+
+  })
+
 }
 
 match_data('./parents.json', './childrens.json')
